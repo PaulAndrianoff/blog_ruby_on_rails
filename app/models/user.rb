457a29f02d9c1ffa6865a5909342dff8945c44f8
Simon LUCAS <sim.lucas27@gmail.com>
2018-02-16 09:54:51 +0100

    :bug: Register username error

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,7 @@ class User < ApplicationRecord
 
   # Only allow letter, number, underscore and punctuation.
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
-  validate :validate_username
-
-  def validate_username
-    if User.where(username: username).exists? 
-      errors.add(:username, :invalid)
-    end
-  end
+  validates :username, uniqueness: true
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
@@ -28,6 +22,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, 
+         :recoverable, :rememberable, :trackable,
          :validatable, :authentication_keys => [:username]
 end
